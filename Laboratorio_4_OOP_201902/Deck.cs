5d544a0956786e20b9826fb15543ea0a65bbd8c9
JoaquinPBarrientos <53538542+JoaquinPBarrientos@users.noreply.github.com>
2019-09-13 09:10:02 -0300

    Add files via upload

---
+++ Laboratorio_4_OOP_201902/Deck.cs
@@ -0,0 +1,54 @@
+﻿using Laboratorio_4_OOP_201902.Cards;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio_4_OOP_201902
+{
+    public class Deck
+    {
+
+        private List<Card> cards;
+
+        public Deck()
+        {
+        
+        }
+
+        public List<Card> Cards { get => cards; set => cards = value; }
+
+        public void AddCard(Card card)
+        {
+            cards.Add(card);
+         
+        }
+        public void DestroyCard(int cardId)
+        {
+            cards.RemoveAt(cardId);
+           
+        }
+        public void Shuffle()
+        {
+            DesordenarLista(cards); 
+            
+        }
+
+        // Método agregado por mi para revolver/desordenar listas.
+        public static List<T> DesordenarLista<T>(List<T> input)
+        {
+            List<T> arr = input;
+            List<T> arrDes = new List<T>();
+
+            Random randNum = new Random();
+            while (arr.Count > 0)
+            {
+                int val = randNum.Next(0, arr.Count - 1);
+                arrDes.Add(arr[val]);
+                arr.RemoveAt(val);
+            }
+
+            return arrDes;
+        }
+
+        }
+}
